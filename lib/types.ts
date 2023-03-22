8a284e5af632ff20df06918ed18b42a2082918d9
Aaron Huggins <aaron.huggins@runbox.com>
2023-03-22 08:42:24 -0500

    Add AwaitResult helper type

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,9 @@
 declare global {
 	// deno-lint-ignore no-explicit-any
-	const workerCache: Map<any, any>
+	const workerCache: Map<any, any>;
 }
 
 // deno-lint-ignore no-explicit-any
 export type NiceAsync<Params extends any[] = any[], Result = any> = (...args: Params) => Promise<Result>;
+// deno-lint-ignore no-explicit-any
+export type AwaitResult<T extends (...args: any) => any> = Awaited<ReturnType<T>>;
