546ab32ed27b7afd4a777c7fbed50bdb0cd0b10d
Aaron Huggins <aaron.huggins@runbox.com>
2023-03-24 15:22:30 -0500

    Add runtime type check for global worker

--- lib/global_worker.ts
+++ lib/global_worker.ts
@@ -2,10 +2,21 @@ import { NiceWorker } from './NiceWorker.ts';
 
 let globalWorker: typeof NiceWorker = NiceWorker;
 
+// deno-lint-ignore no-explicit-any
+function isLikeNiceWorker(input: any): input is typeof NiceWorker {
+	return typeof input === 'object' && Object.hasOwn(input.prototype, 'postMessage') &&
+		Object.hasOwn(input.prototype, 'addEventListener') && Object.hasOwn(input.prototype, 'removeEventListener') &&
+		Object.hasOwn(input.prototype, 'terminate');
+}
+
 export function getGlobalWorker(): typeof NiceWorker {
 	return globalWorker;
 }
 
 export function setGlobalWorker(workerImpl: typeof NiceWorker): void {
-	globalWorker = workerImpl;
+	if (isLikeNiceWorker(workerImpl)) {
+		globalWorker = workerImpl;
+	}
+
+	throw new TypeError('Function setGlobalWorker: type of input is not suficiently like NiceWorker');
 }
